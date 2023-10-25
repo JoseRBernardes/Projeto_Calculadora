37008ba17cf020141b4ccdf345074bdca6c436ce
JOSE ROBERTO BERNARDES JUNIOR <012100199@hsm.int>
2023-10-25 16:40:58 -0300

    app finalizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.calculadora_aulaprogmobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calculadora_aulaprogmobile"
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -44,4 +48,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("org.mariuszgromada.math:MathParser.org-mXparser:5.2.1")
 }
--- app/src/main/java/com/example/calculadora_aulaprogmobile/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_aulaprogmobile/MainActivity.kt
@@ -2,10 +2,113 @@ package com.example.calculadora_aulaprogmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.calculadora_aulaprogmobile.databinding.ActivityMainBinding
+import org.mariuszgromada.math.mxparser.Expression
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:  ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val calculo = binding.calculo
+
+
+        binding.um.setOnClickListener{
+          calculo.text  = "${calculo.text}1"
+        }
+
+        binding.dois.setOnClickListener{
+            calculo.text  = "${calculo.text}2"
+        }
+
+        binding.tres.setOnClickListener{
+            calculo.text  = "${calculo.text}3"
+        }
+
+        binding.quatro.setOnClickListener{
+            calculo.text  = "${calculo.text}4"
+        }
+
+        binding.cinco.setOnClickListener{
+            calculo.text  = "${calculo.text}5"
+        }
+
+        binding.seis.setOnClickListener{
+            calculo.text  = "${calculo.text}6"
+        }
+
+        binding.sete.setOnClickListener{
+            calculo.text  = "${calculo.text}7"
+        }
+
+        binding.oito.setOnClickListener{
+            calculo.text  = "${calculo.text}8"
+        }
+
+        binding.nove.setOnClickListener{
+            calculo.text  = "${calculo.text}9"
+        }
+
+        binding.zero.setOnClickListener{
+            calculo.text  = "${calculo.text}0"
+        }
+
+        binding.abrirParentese.setOnClickListener{
+            calculo.text  = "${calculo.text}("
+        }
+
+        binding.fecharParentese.setOnClickListener{
+            calculo.text  = "${calculo.text})"
+        }
+
+        binding.adicao.setOnClickListener{
+            calculo.text  = "${calculo.text}+"
+        }
+
+        binding.subtracao.setOnClickListener{
+            calculo.text  = "${calculo.text}-"
+        }
+
+        binding.multiplicacao.setOnClickListener{
+            calculo.text  = "${calculo.text}*"
+        }
+
+        binding.divisao.setOnClickListener{
+            calculo.text  = "${calculo.text}/"
+        }
+
+        binding.deletar.setOnClickListener {
+            calculo.text = calculo.text.dropLast(1)
+        }
+
+        binding.ce.setOnClickListener {
+            calculo.text = ""
+            binding.resultado.text = ""
+        }
+
+        binding.ponto.setOnClickListener {
+            calculo.text = "${calculo.text}."
+        }
+
+        binding.igual.setOnClickListener {
+            val resultadoCalculado = Expression(calculo.text.toString()).calculate()
+
+            if(resultadoCalculado.isNaN()){
+                binding.resultado.text = "Calculo invalido"
+            }else{
+                binding.resultado.text = resultadoCalculado.toString()
+            }
+
+
+
+        }
+
+
+
     }
 }
